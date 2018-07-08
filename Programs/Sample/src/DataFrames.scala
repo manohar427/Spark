3d8f113a44ea340bea3659a314cb7da6f6b61314
Manohar Sambayyapalem <manohar427@gmail.com>
2018-07-08 22:19:35 +0530

    Dataframe example

--- Programs/Sample/src/DataFrames.scala
+++ Programs/Sample/src/DataFrames.scala
@@ -12,7 +12,7 @@ object DataFrames {
     val sc = new SparkContext(sc1);
     val sqlContext = new org.apache.spark.sql.SQLContext(sc)
     val df = sqlContext.read.json("Person.json")
-    import df.sparkSession.implicits._
+  /*  import df.sparkSession.implicits._
 
     println("Json data........... Start");
     df.show(3)
@@ -23,6 +23,13 @@ object DataFrames {
     df.groupBy("age").count().show()
     df.select($"name", $"age" + 1).show()
 
-    df.filter($"age" > 80).show(1000)
+    df.filter($"age" > 80).show(1000)*/
+    
+    df.createOrReplaceTempView("people")
+    
+    val sqlDF = sqlContext.sql("SELECT age as AGE,name as NAME FROM people")
+sqlDF.show()
+//df.createGlobalTempView("people")
+
   }
 }
